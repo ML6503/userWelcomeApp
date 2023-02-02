f82c2c2ac9d600d8256d63b1ac19dc7a756d10b1
ML6503 <ml6503@gmail.com>
2023-02-02 13:19:55 +0200

    feat: add test for App

--- client/src/__tests__/App.test.tsx
+++ client/src/__tests__/App.test.tsx
@@ -4,23 +4,24 @@ import React from 'react';
 import App from '../components/App';
 
 describe('App when render', () => {
-  const useState = jest.fn();
-
-  it('should pass it to input value on change', async () => {
+  it('should display Log into account form and after click on sign up - display Sign Up form', async () => {
     render(<App />);
     const loader = screen.getByTestId('loader');
     expect(loader).toBeInTheDocument();
 
     expect(screen.queryByText(/Log into your account/)).toBeNull();
-
-    // const logMsg = screen.getByText('Log into your account');
-    // expect(logMsg).toBeInTheDocument();
-    // const clickSignupMsg = screen.getByText('Click to signup');
-    // expect(clickSignupMsg).toBeInTheDocument();
-    // fireEvent.click(clickSignupMsg);
-    // const clickLoginMsg = screen.getByText('Click to login');
-    // expect(clickLoginMsg).toBeInTheDocument();
+    expect(await screen.findByText(/Log into your account/)).toBeInTheDocument();
+    expect(await screen.findByText(/e-mail:/)).toBeInTheDocument();
+    expect(await screen.findByText(/password:/)).toBeInTheDocument();
+    expect(await screen.findByText(/Login/)).toBeInTheDocument();
+    expect(await screen.findByText(/New User?/)).toBeInTheDocument();
+    expect(await screen.findByText(/Login/)).toBeInTheDocument();
+    expect(await screen.findByText(/Click to signup/)).toBeInTheDocument();
+    fireEvent.click(await screen.findByText(/Click to signup/));
+    expect(screen.queryByText(/Click to signup/)).toBeNull();
+    expect(await screen.findByText(/Getting started/)).toBeInTheDocument();
+    expect(await screen.findByText(/full name:/)).toBeInTheDocument();
+    expect(await screen.findByText(/Existing User?/)).toBeInTheDocument();
+    expect(await screen.findByText(/Click to login/)).toBeInTheDocument();
   });
 });
-
-// https://www.robinwieruch.de/react-testing-library/
