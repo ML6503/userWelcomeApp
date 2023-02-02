034b5b0032889165788a557da6191155688566d0
ML6503 <ml6503@gmail.com>
2023-02-02 16:53:03 +0200

    init test mock class call in auth

---
+++ client/src/__tests__/Auth.test.tsx
@@ -0,0 +1,38 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
+import Auth from '../components/Auth';
+import UserStore from '../store/userStore';
+import AuthController from '../controllers/authController';
+
+describe('EmailInput when render with email prop', () => {
+  beforeEach(() => jest.clearAllMocks());
+  it('should pass it to input value on change', async () => {
+    const user = new UserStore();
+    const mockedSetUser = jest.spyOn(user, 'setUser');
+    const mockedIsAuth = jest.spyOn(user, 'setIsAuth');
+    mockedSetUser.mockImplementation(() => {});
+    mockedIsAuth.mockImplementation(() => {});
+
+    const authController = new AuthController();
+    const mockedToRegister = jest.spyOn(authController, 'toRegister');
+    const mockedToLogin = jest.spyOn(authController, 'toLogin');
+    mockedToRegister.mockImplementation((name, email, password) => Promise.resolve());
+
+    mockedToLogin.mockImplementation((email, password) => Promise.resolve());
+
+    render(<Auth userStore={user} />);
+
+    const form = screen.getByTestId('form');
+    expect(form).toBeInTheDocument();
+
+    fireEvent.change(screen.getByTestId('input-password'), {
+      target: { value: 'SuperTestpasrd1$$$11_' },
+    });
+    fireEvent.change(screen.getByTestId('input-email'), { target: { value: 'test@email.com' } });
+
+    fireEvent.submit(form);
+
+    screen.debug();
+    expect(mockedToLogin).toHaveBeenCalledTimes(1);
+  });
+});
